f7bef5f56f5e9c66094dd43e3c0b499b5e39953c
Ryan Hiebert <ryan@ryanhiebert.com>
2015-12-13 00:11:24 -0600

    Get tests working with tox

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='backports.csv',
+    description='Backport of Python 3 csv module',
+    author='Ryan Hiebert',
+    author_email='ryan@ryanhiebert.com',
+    url='https://github.com/ryanhiebert/backports.csv',
+    version='1.0',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Progrmmming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Lnaguage :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
