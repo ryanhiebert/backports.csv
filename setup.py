c8f45f2419146ff69da90944aeb27544be9df1dd
Ryan Hiebert <ryan@ryanhiebert.com>
2016-02-10 22:59:18 -0600

    Append the HISTORY to the README

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+
+def fread(fn):
+    return open(fn, 'rb').read().decode('utf-8')
+
 setup(
     name='backports.csv',
     description='Backport of Python 3 csv module',
-    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    long_description=fread('README.rst') + '\n\n' + fread('HISTORY.rst'),
     author='Ryan Hiebert',
     author_email='ryan@ryanhiebert.com',
     url='https://github.com/ryanhiebert/backports.csv',
