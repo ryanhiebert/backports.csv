69e64fd5c0d769d13b2e3d5c281046b9c10c932a
Ryan Hiebert <ryan@ryanhiebert.com>
2019-03-10 18:30:33 -0500

    Test under Python 3.7 (#35)

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     package_dir={'': 'src'},
 )
